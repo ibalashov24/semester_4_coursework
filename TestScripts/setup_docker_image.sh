3dee994c4dab11ff625800fbfb1aecc14a5e7e49
Ilya Balashov <ibalashov24@gmail.com>
2019-04-13 02:44:06 +0300

    Fix bug with echo

--- TestScripts/setup_docker_image.sh
+++ TestScripts/setup_docker_image.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 
 function build_docker_image {
-    # Setting up docker image
-    echo "docker build https://github.com/anastasiia-kornilova/epicbox-images.git#xenial:/epicbox-trik -f Dockerfile.xenial --label checker"
-    echo "docker volume create trik-checker-sandbox"
+    echo "Setting up docker image"
+    docker build https://github.com/anastasiia-kornilova/epicbox-images.git#xenial:/epicbox-trik -f Dockerfile.xenial --label checker
+    docker volume create trik-checker-sandbox
     volume_path=$(docker volume inspect trik-checker-sandbox --format '{{.Mountpoint}}')
     
-    # Setting up checker stuff
-    echo "svn checkout https://github.com/ibalashov24/semester_4_coursework/trunk/MapGenerator $volume_path"
-    echo "svn checkout https://github.com/ibalashov24/semester_4_coursework/trunk/TestScripts $volume_path"
+    echo "Setting up checker stuff"
+    svn checkout https://github.com/ibalashov24/semester_4_coursework/trunk/MapGenerator $volume_path
+    svn checkout https://github.com/ibalashov24/semester_4_coursework/trunk/TestScripts $volume_path
     
-    # Copying problem solution
-    echo "cp ./solution.js $volume_path/solution.js"
+    echo "Copying problem solution"
+    cp ./solution.js $volume_path/solution.js
     
-    # Launching Docker container
-    echo "docker run checker:latest --name trik-checker -v trik-checker-sandbox:/trikStudio-checker/ 'apt update && apt install python3 -y'"
-    echo "docker run checker:latest --name trik-checker -v trik-checker-sandbox:/trikStudio-checker/ 'cd trikStudio-checker && python3 solution_tester.py'"
+    echo "Launching Docker container"
+    docker run checker:latest --name trik-checker -v trik-checker-sandbox:/trikStudio-checker/ 'apt update && apt install python3 -y'
+    docker run checker:latest --name trik-checker -v trik-checker-sandbox:/trikStudio-checker/ 'cd trikStudio-checker && python3 solution_tester.py'
 }
 
 build_docker_image
