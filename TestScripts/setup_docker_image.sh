6d851cced7144293c145b6c8639a4cdda3886034
Ilya Balashov <ibalashov24@gmail.com>
2019-04-16 06:54:03 +0000

    Add docker setup script

--- TestScripts/setup_docker_image.sh
+++ TestScripts/setup_docker_image.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
+set -e
 
-function build_docker_image {
+function prepare_docker_image {
     echo "Setting up docker image"
     docker build https://github.com/anastasiia-kornilova/epicbox-images.git#xenial:/epicbox-trik -f Dockerfile.xenial --label checker
-    docker volume create trik-checker-sandbox
+
+    docker volume create trik-checker-sandbox_2
     volume_path=$(docker volume inspect trik-checker-sandbox --format '{{.Mountpoint}}')
+
+    echo $volume_path
     
     echo "Setting up checker stuff"
     svn checkout https://github.com/ibalashov24/semester_4_coursework/trunk/MapGenerator $volume_path
@@ -12,10 +16,14 @@ function build_docker_image {
     
     echo "Copying problem solution"
     cp ./solution.js $volume_path/solution.js
-    
-    echo "Launching Docker container"
-    docker run checker:latest --name trik-checker -v trik-checker-sandbox:/trikStudio-checker/ 'apt update && apt install python3 -y'
-    docker run checker:latest --name trik-checker -v trik-checker-sandbox:/trikStudio-checker/ 'cd trikStudio-checker && python3 solution_tester.py'
 }
 
-build_docker_image
+function run_testing {
+    command='apt update && apt install python3 -y && cd trikStudio-checker && python3 solution_tester.py'
+    
+    echo "Launching Docker container"
+    docker run checker:latest --name trik-checker2 -v trik-checker-sandbox_2:/trikStudio-checker/ "$command"
+}
+
+prepare_docker_image
+run_testing
